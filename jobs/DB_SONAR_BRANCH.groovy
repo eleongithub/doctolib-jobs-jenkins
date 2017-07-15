ccf1c30b4a5b88695dffb7c3b743b94e4861bf15
Eric LEGBA <eleongithub@gmail.com>
2017-07-15 17:46:54 +0200

    Creation du job de lancement d'un scan sonar

---
+++ jobs/DB_SONAR_BRANCH.groovy
@@ -0,0 +1,34 @@
+
+// Job d'exécution des tests Junit et transfert des résultats à Sonar
+def job = mavenJob('DB_SONAR_BRANCH'){
+
+//  Vider le repertoire de travail avant de lancer un nouveau build
+    wrappers {
+        colorizeOutput()
+        preBuildCleanup()
+    }
+
+    // Définir le JDK par défaut
+
+    // Description du job.
+    description('Ce job permet de lancer un scan sonar sur une branche de  l\'application doctolib')
+
+
+//    Définir les paramètres du Job
+    parameters {
+        stringParam('branch', 'master', 'Branche à utiliser pour lancer un scan sonar')
+        // TODO - à integrer liste dynamique des branches. Example : http://www.nachum234.com/automation/jenkins/dynamically-list-git-branches-in-jenkins-parameter/
+    }
+
+//    Récupérer sur Git la branche à utiliser pour faire le snapshot
+    scm {
+        git {
+            remote {
+                url('https://github.com/eleongithub/doctolib.git')
+            }
+            branch('${branch}')
+        }
+    }
+
+    goals('sonar:sonar -Dsonar.host.url=http://192.168.1.97:9000')
+}
