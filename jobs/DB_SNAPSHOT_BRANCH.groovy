7c3929e42834eab95ba19bbc1dd35066789c4c77
Eric LEGBA <eleongithub@gmail.com>
2017-07-15 17:46:16 +0200

    Amelioration du job des snapshots

--- jobs/DB_SNAPSHOT_BRANCH.groovy
+++ jobs/DB_SNAPSHOT_BRANCH.groovy
@@ -1,22 +1,23 @@
 
+// Job de déploiement d'une version snapshots de l'application sur le repository Nexus
 def job = mavenJob('DB_SNAPSHOT_BRANCH'){
 
-//  Vider le repertoire de travail avant de lancer le build maven
+//  Vider le repertoire de travail avant de lancer un nouveau build
     wrappers {
+        colorizeOutput()
         preBuildCleanup()
     }
 
     //    Définir le JDK par défaut
 
     // Description du job.
-    description('Ce job permet de lancer un snapshot de lapplication doctolib')
+    description('Ce job permet de lancer un snapshot de l\'application doctolib')
 
 
 //    Définir les paramètres du Job
     parameters {
         stringParam('branch', 'master', 'Branche à utiliser pour effectuer le snapshot')
-        // TODO - à integrer plus tard !!
-//        http://www.nachum234.com/automation/jenkins/dynamically-list-git-branches-in-jenkins-parameter/
+//        TODO - à integrer liste dynamique des branches. Example : http://www.nachum234.com/automation/jenkins/dynamically-list-git-branches-in-jenkins-parameter/
 //        activeChoiceReactiveParam('CHOICE-1') {
 //            description('Allows user choose from multiple choices')
 //            filterable()
@@ -43,7 +44,4 @@ def job = mavenJob('DB_SNAPSHOT_BRANCH'){
     }
 
     goals('clean deploy -DskipTests')
-    properties{
-        skipTests:true
-    }
 }
