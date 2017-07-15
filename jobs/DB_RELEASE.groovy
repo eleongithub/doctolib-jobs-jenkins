218c6eb1c6e592065391492724617881f9db8a2f
Eric LEGBA <eleongithub@gmail.com>
2017-07-15 19:15:11 +0200

    Job maven pour effectuer une release de l'application doctolib

---
+++ jobs/DB_RELEASE.groovy
@@ -0,0 +1,39 @@
+
+// Job de création d'une version de release stable de l'application et upload des livrables sur le repository Nexus
+def job = mavenJob('DB_RELEASE'){
+
+//  Vider le repertoire de travail avant de lancer un nouveau build
+    wrappers {
+        colorizeOutput()
+        preBuildCleanup()
+    }
+
+    //    Définir le JDK par défaut
+
+    // Description du job.
+    description('Ce job permet de créer une version release stable de l\'application doctolib')
+
+
+//    Définir les paramètres du Job
+    parameters {
+        stringParam('branch', 'master', 'Branche à utiliser pour effectuer la release')
+//        TODO - à integrer liste dynamique des branches. Example : http://www.nachum234.com/automation/jenkins/dynamically-list-git-branches-in-jenkins-parameter/
+//        TODO - https://jenkinsci.github.io/job-dsl-plugin/#method/javaposse.jobdsl.dsl.helpers.BuildParametersContext.activeChoiceReactiveParam
+        stringParam('releaseVersion', '', 'Version de release à produire.') // TODO - How to make this string param validating input ?
+        stringParam('developmentVersion', '', 'Version de developpement à initialiser après le release') // TODO - How to make this string param validating input ?
+
+
+    }
+
+//    Récupérer sur Git la branche à utiliser pour faire le snapshot
+    scm {
+        git {
+            remote {
+                url('https://github.com/eleongithub/doctolib.git')
+            }
+            branch('${branch}')
+        }
+    }
+//    Liste des goals pour réaliser la release
+    goals('release:clean release:prepare release:perform  -DreleaseVersion=${releaseVersion} -DdevelopmentVersion=${developmentVersion} -DskipTests')
+}
